61968c78f1f1e3769951afcc6f93ca5699aa0ee7
wenazo <jslacunza@gmail.com>
2020-10-30 17:35:19 -0300

    Funcion until agregada

--- incl/js/app.js
+++ incl/js/app.js
@@ -10,18 +10,18 @@ if (!window.localStorage.getItem('isBooted')) {
 }
 
 // On click
-document.getElementById('addTimeOnce').onclick = function(){ addOne('countTime', 'countTime'); };
-document.getElementById('addTimeTen').onclick = function(){ addTen('countTime', 'countTime'); };
-document.getElementById('resetTime').onclick = function(){ resetCounter('countTime','countTime'); };
-document.getElementById('addPermOnce').onclick = function(){ addOne('countPerm', 'countPerm'); };
-document.getElementById('addPermTen').onclick = function(){ addTen('countPerm', 'countPerm'); };
-document.getElementById('resetPerm').onclick = function(){ resetCounter('countPerm','countPerm'); };
-document.getElementById('addWeaponOnce').onclick = function(){ addOne('countWeapon', 'countWeapon'); };
-document.getElementById('addWeaponTen').onclick = function(){ addTen('countWeapon', 'countWeapon'); };
-document.getElementById('resetWeapon').onclick = function(){ resetCounter('countWeapon', 'countWeapon'); };
+document.getElementById('addTimeOnce').onclick = function(){ addOne('countTime', 'countTime', 'untilTime'); };
+document.getElementById('addTimeTen').onclick = function(){ addTen('countTime', 'countTime', 'untilTime'); };
+document.getElementById('resetTime').onclick = function(){ resetCounter('countTime','countTime', 'untilTime'); };
+document.getElementById('addPermOnce').onclick = function(){ addOne('countPerm', 'countPerm', 'untilPerm'); };
+document.getElementById('addPermTen').onclick = function(){ addTen('countPerm', 'countPerm', 'untilPerm'); };
+document.getElementById('resetPerm').onclick = function(){ resetCounter('countPerm','countPerm', 'untilPerm'); };
+document.getElementById('addWeaponOnce').onclick = function(){ addOne('countWeapon', 'countWeapon', 'untilWeapon'); };
+document.getElementById('addWeaponTen').onclick = function(){ addTen('countWeapon', 'countWeapon', 'untilWeapon'); };
+document.getElementById('resetWeapon').onclick = function(){ resetCounter('countWeapon', 'countWeapon', 'untilWeapon'); };
 
 // Add one to counter
-function addOne(item, element) {
+function addOne(item, element, until) {
     let currentPos = parseInt(window.localStorage.getItem(item));
     currentPos++;
     if (currentPos > 89) {
@@ -29,9 +29,10 @@ function addOne(item, element) {
     }
     window.localStorage.setItem(item, currentPos);
     document.getElementById(element).textContent = currentPos;
+    document.getElementById(until).textContent = (90-currentPos);
 }
 // Add ten to counter
-function addTen(item, element) {
+function addTen(item, element, until) {
     let currentPos = parseInt(window.localStorage.getItem(item));
     currentPos += 10;
     if (currentPos > 89) {
@@ -39,16 +40,22 @@ function addTen(item, element) {
     }
     window.localStorage.setItem(item, currentPos);
     document.getElementById(element).textContent = currentPos;
+    document.getElementById(until).textContent = (90-currentPos);
 }
 // Reset counter
-function resetCounter(item, element) {
+function resetCounter(item, element, until) {
     window.localStorage.setItem(item, 0);
     document.getElementById(element).textContent = window.localStorage.getItem(item);
+    document.getElementById(until).textContent = 90;
 }
 
 // load Content
 function loadContent() {
     document.getElementById('countTime').textContent = window.localStorage.getItem('countTime');
+    document.getElementById('untilTime').textContent = (90 - window.localStorage.getItem('countTime'));
     document.getElementById('countPerm').textContent = window.localStorage.getItem('countPerm');
+    document.getElementById('untilPerm').textContent = (90 - window.localStorage.getItem('countPerm'));
     document.getElementById('countWeapon').textContent = window.localStorage.getItem('countWeapon');
+    document.getElementById('untilWeapon').textContent = (90 - window.localStorage.getItem('countWeapon'));
+
 }
